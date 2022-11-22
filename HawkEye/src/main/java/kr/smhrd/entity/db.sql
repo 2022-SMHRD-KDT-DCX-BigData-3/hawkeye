eeb9eff8437b794268367004cb11e019a2f74321
crismoon@naver.com <crismoon@naver.com>
2022-11-22 16:35:27 +0900

    db연결, 최종 수정 22일 4시 35분

--- HawkEye/src/main/java/kr/smhrd/entity/db.sql
+++ HawkEye/src/main/java/kr/smhrd/entity/db.sql
@@ -33,3 +33,31 @@ insert into tblMember values('smhrd02','smhrd02','홍길동');
 insert into tblMember values('smhrd03','smhrd03','허아름');
 
 select * from tblMember;
+
+
+
+CREATE TABLE member
+(
+    id VARCHAR2(20) NOT NULL,
+    pw VARCHAR2(20) NOT NULL,
+    nick VARCHAR2(20) NOT NULL,
+    CONSTRAINT id_pk PRIMARY KEY (id)
+);
+
+drop table member;
+
+
+create table Board
+(idx number,
+title varchar2(20),
+writer varchar2(20),
+content varchar2(200),
+constraint idx_pk primary key (idx)
+);
+
+
+insert into member values('crismoon','aa','아기곰');
+
+select * from member;
+
+
