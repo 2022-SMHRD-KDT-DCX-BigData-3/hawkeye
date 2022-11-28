a87b34bfce551886a7a75438e354a513c3589604
user <user@DESKTOP-RF3H2VG>
2022-11-28 16:20:18 +0900

    회원가입 로그인 완료

---
+++ src/main/java/kr/smhrd/controller/MemberController.java
@@ -0,0 +1,50 @@
+package kr.smhrd.controller;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import kr.smhrd.domain.MemberRepository;
+import kr.smhrd.entity.Member;
+
+@Controller
+public class MemberController {
+	
+	//private List<Member> members = new ArrayList<Member>();
+	
+	@Autowired
+	private MemberRepository memberRepository;
+	
+	@PostMapping("/create")
+	public String create(Member member) {
+		System.out.println("userid :" + member); 
+		memberRepository.save(member);
+		return "main";
+	}
+	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String login_id, String login_pw, HttpSession session) {
+		Member member = memberRepository.findByMemid(login_id);
+		if (member == null) {
+		return "redirect:users/loginForm";
+		}
+		if (!login_pw.equals(member.getMempw())) {
+			return "redirect:/users/loginForm";
+		}
+		
+		session.setAttribute("member", member);
+		return "redirect:/main";
+	}
+
+}
+ 
