58ba00a9b5f673c39040e596183f44f08b5ebe30
user <user@DESKTOP-RF3H2VG>
2022-12-06 15:27:37 +0900

    업로드관련 푸쉬입니다

---
+++ src/main/java/kr/smhrd/controller/UplodeController.java
@@ -0,0 +1,8 @@
+package kr.smhrd.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UplodeController {
+	
+}
--- src/main/java/kr/smhrd/entity/Photo.java
+++ src/main/java/kr/smhrd/entity/Photo.java
@@ -2,7 +2,11 @@ package kr.smhrd.entity;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import lombok.Data;
 
 @Data
@@ -14,6 +18,11 @@ public class Photo {
 	private Date cr_date;
 	private String admin_id;
 	private int cr_count;
+	
+	
+	@Column(columnDefinition = "date default sysdate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date mem_joindate=new Date();   
 	
 }
 
